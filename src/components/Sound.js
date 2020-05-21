b89d454fd61b76f8176f0ad58e2ad755c06f0c01
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-05-21 20:22:26 +0100

    Sounds are preloaded

---
+++ src/components/Sound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import A from '../sounds/a3 64.mp3';
+import B from '../sounds/b3 64.mp3';
+import C from '../sounds/c3 64.mp3';
+import D from '../sounds/d3 64.mp3';
+import E from '../sounds/e3 64.mp3';
+import F from '../sounds/f3 64.mp3';
+import G from '../sounds/g3 64.mp3';
+
+const soundDictionary = {
+    'A': A,
+    'B': B,
+    'C': C,
+    'D': D,
+    'E': E,
+    'F': F,
+    'G': G,
+}
+
+export default soundDictionary
